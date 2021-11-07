b0878f2f9cf041e0b45419647e328fcfb644161e
Sizertt <93645062+Sizertt@users.noreply.github.com>
2021-11-07 07:03:27 +0300

    BlockDemo

---
+++ src/BlockDemo.java
@@ -0,0 +1,19 @@
+public class BlockDemo {
+    public static void main(String[] args) {
+
+        double i, j, d;
+
+        i = 50;
+        j = 150;
+
+        if(i != 0 ) {
+
+        System.out.println("Делитель не равен нулю");
+        d=j / i;
+        System.out.println("j / i равно " + d);
+
+    } // if (i != 0)
+
+    } // main(String[])
+
+} // BlockDemo class
